366ef1faf79b97d274e65a366bf5bf49617679f1
Michael Thorne <michael.thorne@gmail.com>
2016-08-14 16:53:45 +0200

    WIP

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,22 @@
 var gulp = require('gulp');
+var clean = require('gulp-clean');
+var sass = require('gulp-sass');
+var watch = require('gulp-watch');
 
-gulp.task('default', function() {
-    // place code for your default task here
+gulp.task('clean', function () {
+    return gulp.src('build', {read: false})
+        .pipe(clean());
 });
+
+gulp.task('sass', function () {
+    return gulp.src('src/sass/style.scss')
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(gulp.dest('build/css'));
+});
+
+gulp.task('watch', function () {
+    return gulp.watch('src/sass/**/*.scss', ['sass'])
+});
+
+gulp.task('build', ['clean', 'sass']);
+gulp.task('default', ['build', 'watch']);
