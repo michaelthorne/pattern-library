f1e3b687d7803f7416dc82896221a492695f51ff
Michael Thorne <michael.thorne@gmail.com>
2016-08-14 18:50:02 +0200

    WIP

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,48 @@
 var gulp = require('gulp');
-var clean = require('gulp-clean');
+var connect = require('gulp-connect');
+var del = require('del');
+var runSequence = require('run-sequence');
 var sass = require('gulp-sass');
-var watch = require('gulp-watch');
 
-gulp.task('clean', function () {
-    return gulp.src('build', {read: false})
-        .pipe(clean());
+gulp.task('clean:build', function () {
+    del(['build']);
 });
 
-gulp.task('sass', function () {
+gulp.task('connect', function () {
+    connect.server({
+        root: 'build',
+        livereload: true,
+        port: 1337
+    });
+});
+
+gulp.task('copy:build', function () {
+    return gulp.src(['src/**.html', 'src/apple-touch-icon.png', 'src/favicon.ico', 'src/humans.txt', 'src/robots.txt'])
+        .pipe(gulp.dest('build'))
+        .pipe(connect.reload());
+});
+
+gulp.task('html', function () {
+    gulp.src('./app/*.html')
+        .pipe(connect.reload());
+});
+
+gulp.task('sass:build', function () {
     return gulp.src('src/sass/style.scss')
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(gulp.dest('build/css'));
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('build/css'))
+        .pipe(connect.reload());
 });
 
 gulp.task('watch', function () {
-    return gulp.watch('src/sass/**/*.scss', ['sass'])
+    gulp.watch('src/sass/**/*.scss', ['sass:build']);
+    gulp.watch('src/**/*.html', ['copy:build']);
 });
 
-gulp.task('build', ['clean', 'sass']);
-gulp.task('default', ['build', 'watch']);
+gulp.task('build', function () {
+    runSequence('clean:build', ['copy:build', 'sass:build']);
+});
+
+gulp.task('default', function () {
+    runSequence('build', ['connect', 'watch']);
+});
